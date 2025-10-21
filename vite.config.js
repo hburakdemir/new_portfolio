db0c2486b7988913d99c106b1c8dbf6f8a61dd0f
Hakan Burak Demir <burakd279@gmail.com>
2025-10-21 12:05:01 +0300

    678

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', 
+  base: '/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js'
   },
-    build: {
+  build: {
     outDir: 'dist'
   }
 })
